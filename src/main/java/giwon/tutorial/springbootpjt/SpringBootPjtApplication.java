fad876b95d67f0b4c212445db5e7def52e931596
GiwonNam <gullivernam94@gmail.com>
2022-05-01 15:21:52 +0900

    init commit

---
+++ src/main/java/giwon/tutorial/springbootpjt/SpringBootPjtApplication.java
@@ -0,0 +1,13 @@
+package giwon.tutorial.springbootpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  SpringBootPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/giwon/tutorial/springbootpjt/SpringBootPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package giwon.tutorial.springbootpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
